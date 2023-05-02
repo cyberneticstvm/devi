678cde1a1a3837a739ab2630a2137016fd39abb6
Vijoy Sasidharan <devuinfosys@gmail.com>
2023-05-02 10:00:45 +0530

    small updates

--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -31,7 +31,7 @@
                     </li>
                     <li class="menu-item">
                         <a href="/branch" class="menu-link">
-                            <i class="menu-icon tf-icons bx bx-home"></i>
+                            <i class="menu-icon tf-icons bx bx-sitemap"></i>
                             <div data-i18n="Branch Management">Branch Management</div>
                         </a>
                     </li>
@@ -40,7 +40,158 @@
                             <i class="menu-icon tf-icons bx bx-user"></i>
                             <div data-i18n="User Management">User Management</div>
                         </a>
+                    </li>
+                    <li class="menu-item">
+                        <a href="/dash" class="menu-link">
+                            <i class="menu-icon tf-icons bx bx-heading"></i>
+                            <div data-i18n="I & E Heads">I & E Heads</div>
+                        </a>
+                    </li>
+                    <li class="menu-item">
+                        <a href="/dash" class="menu-link">
+                            <i class="menu-icon tf-icons bx bx-dollar"></i>
+                            <div data-i18n="Income">Income</div>
+                        </a>
+                    </li>
+                    <li class="menu-item">
+                        <a href="/dash" class="menu-link">
+                            <i class="menu-icon tf-icons bx bxs-credit-card"></i>
+                            <div data-i18n="Expense">Expense</div>
+                        </a>
                     </li>                    
+                </ul>
+            </li>
+
+            <!-- Inventory -->
+            <li class="menu-item">
+                <a href="javascript:void(0)" class="menu-link menu-toggle">
+                    <i class="menu-icon tf-icons bx bx-package"></i>
+                    <div data-i18n="Inventory">Inventory</div>
+                </a>
+                <ul class="menu-sub">                    
+                    <li class="menu-item">
+                        <a href="/branch" class="menu-link">
+                            <i class="menu-icon tf-icons bx bx-checkbox"></i>
+                            <div data-i18n="Category Management">Category Management</div>
+                        </a>
+                    </li>
+                    <li class="menu-item">
+                        <a href="/user" class="menu-link">
+                            <i class="menu-icon tf-icons bx bx-checkbox-square"></i>
+                            <div data-i18n="Subcategory Management">Subcategory Management</div>
+                        </a>
+                    </li>
+                    <li class="menu-item">
+                        <a href="javascript:void(0);" class="menu-link menu-toggle">
+                            <i class="menu-icon tf-icons bx bx-gift"></i>
+                            <div data-i18n="Product Management">Product Management</div>
+                        </a>
+                        <ul class="menu-sub">
+                          <li class="menu-item">
+                            <a href="/dash" class="menu-link">
+                              <div data-i18n="Lens">Lens</div>
+                            </a>
+                          </li>
+                          <li class="menu-item">
+                            <a href="/dash" class="menu-link">
+                              <div data-i18n="Frame">Frame</div>
+                            </a>
+                          </li>
+                          <li class="menu-item">
+                            <a href="/dash" class="menu-link">
+                              <div data-i18n="Medicine">Medicine</div>
+                            </a>
+                          </li>
+                        </ul>
+                    </li>
+                    <li class="menu-item">
+                        <a href="/role" class="menu-link">
+                            <i class="menu-icon tf-icons bx bx-cart-download"></i>
+                            <div data-i18n="Purchase">Purchase</div>
+                        </a>
+                    </li>
+                    <li class="menu-item">
+                        <a href="/role" class="menu-link">
+                            <i class="menu-icon tf-icons bx bx-transfer"></i>
+                            <div data-i18n="Stock Transfer">Stock Transfer</div>
+                        </a>
+                    </li>                    
+                </ul>
+            </li>
+
+            <!-- Patient -->
+            <li class="menu-item">
+                <a href="javascript:void(0)" class="menu-link menu-toggle">
+                    <i class="menu-icon tf-icons bx bxs-user-plus"></i>
+                    <div data-i18n="Registration">Registration</div>
+                </a>
+                <ul class="menu-sub">
+                    <li class="menu-item">
+                        <a href="/dash" class="menu-link">
+                            <i class="menu-icon tf-icons bx bx-user-check"></i>
+                            <div data-i18n="Patient">Patient</div>
+                        </a>
+                    </li>
+                    <li class="menu-item">
+                        <a href="/dash" class="menu-link">
+                            <i class="menu-icon tf-icons bx bxs-user-account"></i>
+                            <div data-i18n="Appointment">Appointment</div>
+                        </a>
+                    </li>                   
+                </ul>
+            </li>
+
+            <!-- Order -->
+            <li class="menu-item">
+                <a href="javascript:void(0)" class="menu-link menu-toggle">
+                    <i class="menu-icon tf-icons bx bx-cart-add"></i>
+                    <div data-i18n="Order">Order</div>
+                </a>
+                <ul class="menu-sub">
+                    <li class="menu-item">
+                        <a href="/dash" class="menu-link">
+                            <i class="menu-icon tf-icons bx bx-glasses"></i>
+                            <div data-i18n="Specs">Specs</div>
+                        </a>
+                    </li>
+                    <li class="menu-item">
+                        <a href="/dash" class="menu-link">
+                            <i class="menu-icon tf-icons bx bxs-capsule"></i>
+                            <div data-i18n="Medicine">Medicine</div>
+                        </a>
+                    </li>                   
+                </ul>
+            </li>
+
+            <!-- Reports -->
+            <li class="menu-item">
+                <a href="javascript:void(0)" class="menu-link menu-toggle">
+                    <i class="menu-icon tf-icons bx bx-file"></i>
+                    <div data-i18n="Reports">Reports</div>
+                </a>
+                <ul class="menu-sub">
+                    <li class="menu-item">
+                        <a href="/dash" class="menu-link">
+                            <i class="menu-icon tf-icons bx bx-file"></i>
+                            <div data-i18n="Daybook">Daybook</div>
+                        </a>
+                    </li>                  
+                </ul>
+            </li>
+
+            <!-- Settings -->
+            <li class="menu-item">
+                <a href="javascript:void(0)" class="menu-link menu-toggle">
+                    <i class="menu-icon tf-icons bx bx-cog"></i>
+                    <div data-i18n="Settings">Settings</div>
+                </a>
+                <ul class="menu-sub">
+                    <li class="menu-item">
+                        <a href="/dash" class="menu-link">
+                            <i class="menu-icon tf-icons bx bx-mobile"></i>
+                            <div data-i18n="Device">Device</div>
+                        </a>
+                    </li>                  
                 </ul>
             </li>
         </ul>

405b5d00ba42700382ef042addd38f315d7f262a
Vijoy Sasidharan <devuinfosys@gmail.com>
2023-10-12 17:14:50 +0530

    small updates

--- app/Models/Transfer.php
+++ app/Models/Transfer.php
@@ -4,8 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Transfer extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $guarded = [];
+
+    public function status()
+    {
+        return ($this->deleted_at) ? "<span class='badge badge-danger'>Deleted</span>" : "<span class='badge badge-success'>Active</span>";
+    }
 }
